1b37631836c522a0d6f62bf10fa91c0ef2cf4c98
Vipul Chandola <vipulchandola3@gmail.com>
2022-09-22 15:14:41 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-
-
-import { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Parent from './bodybuilding/Parent';
-import Navbar from './comp/Navbar';
-import Gallery from './food/Gallery';
-import Navbars from './Nav/Navbars';
+import "./App.css";
+import Parent from "./bodybuilding/Parent";
 
 function App() {
-//   const options = {
-   
-//     headers: {
-//       'X-RapidAPI-Key': 'b1f94988damshed5de3ef6dc6be6p130554jsn03034e63ffd8',
-//       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-//     }
-//   };
-//   useEffect(()=>{
-// const a=async()=>{
-//   let b=await fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',options)
-//   let res= await b.json()
-//   console.log(res)
-// }
-// a()
-//   },[])
   return (
     <>
- 
-<Parent/>
- <BrowserRouter>
- <Navbars/> 
-   <Navbar/> 
- </BrowserRouter>
+      <Parent />
     </>
- 
   );
 }
 
--- src/food/Button.js
+++ src/food/Button.js
@@ -1,41 +1,36 @@
-import React from 'react'
+import React from "react";
 import { Images } from "./Images";
-export default function Button({filteritem,setfirst}) {
-  
+export default function Button({ filteritem, setfirst }) {
   return (
     <>
-    {/* {btn.map((e)=>{
-        return ()
-    })} */}
-    <button
-          type="button"
-          onClick={() =>setfirst(Images)}
-          className="btn btn-info "
-        >
-          All
-        </button>
-        <button
-          type="button"
-          onClick={() => filteritem("breakfast")}
-          className="btn btn-info mx-5"
-        >
-          brfst
-        </button>
-        <button
-          type="button"
-          onClick={() => filteritem("lunch")}
-          className="btn btn-info"
-        >
-          lunch
-        </button>
-        <button
-          type="button"
-          onClick={() => filteritem("dinner")}
-          className="btn btn-info mx-5"
-        >
-          dinner
-        </button>
+      <button
+        type="button"
+        onClick={() => setfirst(Images)}
+        className="btn btn-info "
+      >
+        All
+      </button>
+      <button
+        type="button"
+        onClick={() => filteritem("breakfast")}
+        className="btn btn-info mx-5"
+      >
+        brfst
+      </button>
+      <button
+        type="button"
+        onClick={() => filteritem("lunch")}
+        className="btn btn-info"
+      >
+        lunch
+      </button>
+      <button
+        type="button"
+        onClick={() => filteritem("dinner")}
+        className="btn btn-info mx-5"
+      >
+        dinner
+      </button>
     </>
-   
-  )
+  );
 }
